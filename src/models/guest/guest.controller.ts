2abf2938440a728a2ab66396978e31aa671febf0
TuneDao <tuananh95ndc@gmail.com>
2024-12-01 02:37:46 +0700

    update for user create guestlist

--- src/models/guest/guest.controller.ts
+++ src/models/guest/guest.controller.ts
@@ -13,6 +13,7 @@ import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { GuestListService } from './guest.service';
 import { CreateGuestListDto } from './dto/create-guest.dto';
 import { UpdateGuestListDto } from './dto/update-guest.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @ApiTags('GuestList')
 @Controller('guest-list')
@@ -20,16 +21,18 @@ import { UpdateGuestListDto } from './dto/update-guest.dto';
 export class GuestListController {
   constructor(private readonly guestListService: GuestListService) {}
 
+  @Public()
   @Post()
   async create(@Body() createGuestListDto: CreateGuestListDto) {
     return this.guestListService.create(createGuestListDto);
   }
 
+  @Public()
   @Get()
-  @ApiQuery({
-    name: 'weddingId',
-    description: 'ID of the wedding to filter guests',
-  })
+  // @ApiQuery({
+  //   name: 'weddingId',
+  //   description: 'ID of the wedding to filter guests',
+  // })
   @ApiQuery({
     name: 'page',
     required: false,
@@ -43,13 +46,13 @@ export class GuestListController {
     example: 10,
   })
   async findAll(
-    @Query('weddingId') weddingId: string,
+    // @Query('weddingId') weddingId: string,
     @Query('page') page = 1,
     @Query('limit') limit = 10,
   ) {
     const pageNumber = Math.max(1, Number(page));
     const limitNumber = Math.max(1, Math.min(100, Number(limit))); // Giới hạn tối đa 100
-    return this.guestListService.findAll(weddingId, pageNumber, limitNumber);
+    return this.guestListService.findAll(pageNumber, limitNumber);
   }
   @Get(':id')
   async findOne(@Param('id') id: string) {
