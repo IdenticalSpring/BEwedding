3c3820af7788f63a8c797b99d97b0a44eb61104e
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-12-20 17:42:17 +0700

    Update paginated.dto.ts

--- src/models/subscription/dto/paginated.dto.ts
+++ src/models/subscription/dto/paginated.dto.ts
@@ -1,16 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsOptional, Min } from 'class-validator';
+import { IsInt, Min, IsOptional } from 'class-validator';
 
 export class PaginationQueryDto {
-    @ApiProperty({ example: 1, description: 'Page number', required: false })
-    @IsInt({ message: 'Page must be an integer' })
-    @Min(1, { message: 'Page must be at least 1' })
+    @ApiProperty({
+        description: 'Page number',
+        example: 1,
+        required: false,
+    })
     @IsOptional()
-    page?: number = 1;
+    page: number;
+    @ApiProperty({
+        description: 'Limit per page',
+        example: 10,
+        required: false,
+    })
+    @IsOptional()
 
-    @ApiProperty({ example: 10, description: 'Number of items per page', required: false })
-    @IsInt({ message: 'Limit must be an integer' })
-    @Min(1, { message: 'Limit must be at least 1' })
-    @IsOptional()
-    limit?: number = 10;
+    limit: number;
 }
