8dc61bac4bdda7a86747d9bbf447f1c143063fd9
qpham4243562 <139883097+qpham4243562@users.noreply.github.com>
2024-12-28 17:46:55 +0700

    fix: chỉnh lại template cho invitation

--- src/models/template-user/template-user.module.ts
+++ src/models/template-user/template-user.module.ts
@@ -5,9 +5,11 @@ import { TemplateUserService } from './template-user.service';
 import { TemplateController } from './template-user.controller';
 import { templateUser } from './entity/template-user.entity';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
+import { Template } from '../template/entity/template.entity';
+import { Invitation_User } from '../invitation_user/entity/invitation.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([templateUser]), CloudinaryModule],
+  imports: [TypeOrmModule.forFeature([templateUser,Template,Invitation_User]), CloudinaryModule],
   controllers: [TemplateController],
   providers: [TemplateUserService],
   exports: [TemplateUserService],
